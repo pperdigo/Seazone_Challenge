402483b625ad6a14bb0c6faa1b6e1669c621d988
pperdigo <perdigao.drapier1234@gmail.com>
2022-03-21 17:35:31 -0300

    Added section about price prediction

--- Q1.py
+++ Q1.py
@@ -1,9 +1,10 @@
 #Import Libraries
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.compose import make_column_selector as selector, ColumnTransformer
+from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate, train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from datetime import date
 import re
 from unicodedata import category
@@ -15,16 +16,6 @@ pd.set_option('display.max_columns', None)
 #Import previous data cleaning function
 from data_cleaning import CleanAndMerge
 
-def featureSelection(df):
-    #Separate features and target
-    categorical_selector = selector(dtype_include = object)
-    numerical_selector = selector(dtype_exclude = object)
-
-    categorical_features = categorical_selector(df)
-    numerical_features = numerical_selector(df)
-
-    return categorical_features, numerical_features
-
 def preProcessing(categorical_features, numerical_features):
     categorical_preprocessor = OneHotEncoder(handle_unknown='ignore')
     numerical_preprocessor = StandardScaler()
@@ -34,30 +25,82 @@ def preProcessing(categorical_features, numerical_features):
     )
     return preprocessor
 
-def trainModel(preprocessor, modelType, features, target):
-    model = make_pipeline(preprocessor, modelType)
-    feature_train, feature_test, target_train, target_test = train_test_split(
-        features, target
+if __name__ == '__main__':
+    #Clean and Merge data
+    _revenue = pd.read_csv('daily_revenue.csv')
+    _listings = pd.read_csv('listings.csv')
+    df = CleanAndMerge(_revenue, _listings)
+
+    #Filter only relevant data
+    df['Month'] = pd.DatetimeIndex(df['date']).month
+    df.dropna(inplace=True)
+    
+    features = df[['Localizacao', 'Categoria', 'Qtde_Quartos', 'Month']]
+    revenue_target = df[['revenue']]
+    price_target = df[['last_offered_price']]
+
+    categorical_features = ['Localizacao', 'Categoria', 'Month']
+    numerical_features = ['Qtde_Quartos']
+
+
+    preprocessor = preProcessing(categorical_features, numerical_features)
+    revenue_model = make_pipeline(preprocessor, LinearRegression())
+    price_model = make_pipeline(preprocessor, LinearRegression())
+    
+    # # Manual train-test split
+    # feature_train = features[features['Qtde_Quartos'] != 2]
+    # feature_test = features[features['Qtde_Quartos'] == 2]
+
+    # revenue_target_train_index = feature_train.index
+    # revenue_target_train = revenue_target[revenue_target.index.isin(revenue_target_train_index)]
+
+    # revenue_target_test_index = feature_test.index
+    # revenue_target_test = revenue_target[revenue_target.index.isin(revenue_target_test_index)]
+
+    # # Automatic train-test split
+    feature_train, feature_test, revenue_target_train, revenue_target_test = train_test_split(
+        features, revenue_target, random_state=42
     )
-    print(feature_train, feature_test, target_train, target_test)
-    _ = model.fit(feature_train, target_train)
-    score = model.score(feature_test, target_test)
-    return score
+    _ = revenue_model.fit(X = feature_train, y = revenue_target_train)
+    revenue_score = revenue_model.score(feature_test, revenue_target_test)
+    print(f'The score of the revenue model on the test data was {revenue_score:.4f}')
+    
+    cv_results = cross_validate(revenue_model, features, revenue_target, cv=5)
+    scores = cv_results["test_score"]
+    print("The mean cross-validation accuracy for the revenue model is: "
+       f"{scores.mean():.4f} +/- {scores.std():.4f}")
+    jur2Q = pd.DataFrame([['JUR', 'MASTER', 2, 3]], columns=['Localizacao', 'Categoria', 'Qtde_Quartos', 'Month'])
+    predicted_revenue = revenue_model.predict(jur2Q)
+    print(f'The predicted value for revenue is: {float(predicted_revenue):.2f}')
+    jur1Q = df.query('Localizacao == "JUR" & Categoria == "MASTER" & Qtde_Quartos == 1 & Month == 3')['revenue'].mean()
+    jur3Q = df.query('Localizacao == "JUR" & Categoria == "MASTER" & Qtde_Quartos == 3 & Month == 3')['revenue'].mean()
+    print(f'The mean revenue for a JURMASTER1Q listing is: {jur1Q:.2f}, and for a JURMASTER3Q is {jur3Q:.2f}')
 
-# if __name__ == 'main':
-#Clean and Merge data
-_revenue = pd.read_csv('daily_revenue.csv')
-_listings = pd.read_csv('listings.csv')
-df = CleanAndMerge(_revenue, _listings)
+    # Automatic train-test split
+    feature_train, feature_test, price_target_train, price_target_test = train_test_split(
+        features, price_target, random_state=42
+    )
 
-#Filter only relevant data
-df['Month'] = pd.DatetimeIndex(df['date']).month
-dff = df[df['Month'] == 3]
+    # # Manual train-test split
+    # price_target_train_index = feature_train.index
+    # price_target_train = price_target[price_target.index.isin(price_target_train_index)]
 
-features = dff[['Localizacao', 'Categoria', 'Capacidade', 'Qtde_Quartos', 'Taxa de Limpeza']]
-target = dff['revenue']
+    # price_target_test_index = feature_test.index
+    # price_target_test = price_target[price_target.index.isin(price_target_test_index)]
 
-categorical_features, numerical_features = featureSelection(dff)
-preprocessor = preProcessing(categorical_features, numerical_features)
-score = trainModel(preprocessor, LinearRegression, features, target)   
-print(score)
+    _ = price_model.fit(X = feature_train, y = price_target_train)
+    price_score = price_model.score(feature_test, price_target_test)
+    print(f'The score of the price model on the test data was {price_score:.4f}')
+    
+    cv_results = cross_validate(price_model, features, price_target, cv=5)
+    scores = cv_results["test_score"]
+    print("The mean cross-validation accuracy for the price model is: "
+       f"{scores.mean():.4f} +/- {scores.std():.4f}")
+    jur2Q = pd.DataFrame([['JUR', 'MASTER', 2, 3]], columns=['Localizacao', 'Categoria', 'Qtde_Quartos', 'Month'])
+    predicted_price = price_model.predict(jur2Q)
+    print(f'The predicted value for price is: {float(predicted_price):.2f}')
+    jur1Q = df.query('Localizacao == "JUR" & Categoria == "MASTER" & Qtde_Quartos == 1 & Month == 3')['last_offered_price'].mean()
+    jur3Q = df.query('Localizacao == "JUR" & Categoria == "MASTER" & Qtde_Quartos == 3 & Month == 3')['last_offered_price'].mean()
+    print(f'The mean price for a JURMASTER1Q listing is: {jur1Q:.2f}, and for a JURMASTER3Q is {jur3Q:.2f}')
+
+    
